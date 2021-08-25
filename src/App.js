36337d5105b09bb1813e33ba47724c03250c4817
Art-mj1 <mjmjpc@naver.com>
2021-08-25 14:25:46 +0900

    08/25 리액트 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//import React from "react";
 
+const arr = [1, 2, 3];
+arr.map((a) => {
+  return a;
+});
+
+//내가 만든 컴포넌트 > app.js > index.js >index.html(화면에 보여지는 문서)
+//react는 페이지 전체가 바뀌는 방식이 아니라 처음 보여지는 문서에서 화면만 바뀌는 형식
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World</h1>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/FirstComponent01.js
@@ -0,0 +1,9 @@
+//App.js로 연결할 커스텀 컴포넌트
+import React from "react";
+
+//컴포넌트의 이름은 파일의 이름이자 함수의 이름이다.
+const FirstComponent01 = () => {
+  return <div></div>;
+};
+
+export default FirstComponent01;
